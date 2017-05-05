4f7a6f2d19f62591b42036754e586d15afb1cd4a
Sara Pereira <sara_sp@live.com.pt>
2017-05-05 19:47:57 +0100

    ja faz a pesquisa dos concelhos e freguesias do porto

--- trabalho.py
+++ trabalho.py
@@ -2,8 +2,12 @@
 import psycopg2
 import sys
 import pprint
+
+#conn.set_client_encoding('LATIN9')
  
 def main():
+
+
 	#Define our connection string
 	conn_string = "dbname='guest' user='guest' host = '127.0.0.1' password=''"
  
@@ -16,16 +20,43 @@ def main():
 	# conn.cursor will return a cursor object, you can use this cursor to perform queries
 	cursor = conn.cursor()
  
-	local(cursor);
+	cria_tabelas(cursor)
+	local(cursor)
+
+	conn.commit()
+	cursor.close()
+	conn.close()
+	
+def cria_tabelas(cursor):
+	cursor.execute("DROP TABLE IF EXISTS dw_stand")
+	cursor.execute("DROP TABLE IF EXISTS dw_local")	
+	cursor.execute("DROP TABLE IF EXISTS dw_tempo")
+	cursor.execute("DROP TABLE IF EXISTS dw_taxi")
+	cursor.execute("DROP TABLE IF EXISTS dw_taxi_services")
+
+	cursor.execute("create table dw_tempo(id int primary key not null, hora int not null, dia int not null, mes int not null)")
+	cursor.execute("create table dw_local(id int primary key not null, stand_id int not null, freguesia text not null, concelho text not null)")
+	cursor.execute("create table dw_taxi(id int primary key not null, n_licenca int not null)")
+	cursor.execute("create table dw_stand(id int primary key not null, nome text not null, lotacao int not null)")
+	cursor.execute("create table dw_taxi_services(id int primary key not null, taxi_id int not null, tempo_id int not null, localI_id int not null, localF_id int not null, nViagens int not null, tempoTotal int not null)")
+
 
 def local(cursor):
-	# execute our Query
-	cursor.execute("select location from taxi_stands limit 1")
- 
-	# retrieve the records from the database
-	records = cursor.fetchall()
- 
-	pprint.pprint(records)
+
+	#Freguesia existentes no Porto
+	cursor.execute("select distinct count(freguesia) from cont_freg_v5 where distrito like 'PORTO'")
+	freguesia = cursor.fetchall()
+
+	#Concelhos existentos no porto
+	cursor.execute("select distinct concelho from cont_freg_v5 where distrito like 'PORTO'")
+	concelho = cursor.fetchall()
+
+	barra = "\d";
+
+	bar = cursor.fetchall()
+	cursor.execute("%s", barra)
+
+	pprint.pprint(bar)
 
 
 if __name__ == "__main__":
