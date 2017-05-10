8274feca3e4c2bb80a1af3905b83e2d7c1358a4d
Sara Pereira <sara_sp@live.com.pt>
2017-05-10 02:29:33 +0100

    adcionei o numero de viagens e o tempo total correto mas apenas esta a dar para um valor porque o cilco explode mas esta bem a logica

--- trabalho.py
+++ trabalho.py
@@ -3,56 +3,35 @@ import psycopg2
 import sys
 import pprint
 import datetime
-
-#conn.set_client_encoding('LATIN9')
  
-def main():
-
-
-	#Define our connection string
-	conn_string = "dbname='guest' user='guest' host = '127.0.0.1' password=''"
- 
-	# print the connection string we will use to connect
-	#print "Connecting to database\n	->%s" % (conn_string)
- 
-	# get a connection, if a connect cannot be made an exception will be raised here
-	conn = psycopg2.connect(conn_string)
- 
-	# conn.cursor will return a cursor object, you can use this cursor to perform queries
-	cursor = conn.cursor()
- 
-	cria_tabelas(cursor)
-
-	transfereTaxiStands(cursor)
-	#dw_local(cursor)
-	#dw_tempo(cursor)
-	dw_services(cursor)
-
-	conn.commit()
-	cursor.close()
-	conn.close()
-	
 def cria_tabelas(cursor):
-	cursor.execute("DROP TABLE IF EXISTS dw_stand")
-	cursor.execute("DROP TABLE IF EXISTS dw_local")	
-	cursor.execute("DROP TABLE IF EXISTS dw_tempo")
-	cursor.execute("DROP TABLE IF EXISTS dw_taxi")
-	cursor.execute("DROP TABLE IF EXISTS dw_taxi_services")
+	cursor.execute("DROP TABLE IF EXISTS dw_stand CASCADE")
+	cursor.execute("DROP TABLE IF EXISTS dw_local CASCADE")	
+	cursor.execute("DROP TABLE IF EXISTS dw_tempo CASCADE")
+	cursor.execute("DROP TABLE IF EXISTS dw_taxi CASCADE")
+	cursor.execute("DROP TABLE IF EXISTS dw_taxi_services CASCADE")
 
-	cursor.execute("create table dw_tempo(id int primary key not null, hora text not null, dia text not null, mes text not null)")
-	cursor.execute("create table dw_local(id int primary key not null, stand_id int not null, freguesia text not null, concelho text not null)")
-	cursor.execute("create table dw_taxi(id int primary key not null, n_licenca int not null)")
-	cursor.execute("create table dw_stand(id int primary key not null, nome text not null, lotacao int null, location text not null)")
-	cursor.execute("create table dw_taxi_services(id int primary key not null, taxi_id int not null, tempo_id int not null, localI_id int not null, localF_id int not null, nViagens int not null, tempoTotal int not null)")
+	
+	cursor.execute("create table dw_stand(stand_id bigserial, nome text not null, lotacao int null, location text not null, PRIMARY KEY (stand_id))")
+
+	#cursor.execute("create table dw_local(local_id bigserial, stand_id int not null, freguesia text not null, concelho text not null, PRIMARY KEY (local_id, stand_id), FOREIGN KEY (stand_id) REFERENCES dw_stand (stand_id) ON DELETE CASCADE)")
+        
+        cursor.execute("create table dw_local(local_id bigserial, stand_id int not null, freguesia text not null, concelho text not null, PRIMARY KEY (local_id, stand_id))")
+
+	cursor.execute("create table dw_tempo(tempo_id bigserial, hora text not null, dia text not null, mes text not null, PRIMARY KEY (tempo_id))")
+
+	cursor.execute("create table dw_taxi(taxi_id bigserial, n_licenca int not null, PRIMARY KEY (taxi_id))")
+
+	#cursor.execute("create table dw_taxi_services(id bigserial, taxi_id int not null, tempo_id int not null, localI_id int not null, nViagens int not null, tempoTotal text not null, PRIMARY KEY (id, taxi_id, tempo_id, localI_id), FOREIGN KEY (taxi_id) REFERENCES dw_taxi (taxi_id) ON DELETE CASCADE, FOREIGN KEY (tempo_id) REFERENCES dw_tempo (tempo_id) ON DELETE CASCADE, FOREIGN KEY (localI_id) REFERENCES dw_local (local_id) ON DELETE CASCADE)")
 
 
-def transfereTaxiStands(cursor):
+        cursor.execute("create table dw_taxi_services(id bigserial, taxi_id int not null, tempo_id int not null, localI_id int not null, nViagens int not null, tempoTotal text not null, PRIMARY KEY (id))")
+
+
+def dw_stand(cursor):
 
 	cursor.execute("select count(*) from taxi_stands")
 	nTuplos = cursor.fetchall()
-
-	cursor.execute("select id from taxi_stands")
-	ids = cursor.fetchall()
 
 	cursor.execute("select name from taxi_stands")
 	nomes = cursor.fetchall()
@@ -61,7 +40,7 @@ def transfereTaxiStands(cursor):
 	coordenadas = cursor.fetchall()
 
 	for i in range(0,nTuplos[0][0]):
-		cursor.execute("insert into dw_stand (id, nome, location) values (%s, %s, %s)", (ids[i][0], nomes[i][0],coordenadas[i][0],))
+		cursor.execute("insert into dw_stand (nome, location) values (%s, %s)", (nomes[i][0],coordenadas[i][0],))
 	
 
 #id, stand_id, freguesia, concelho
@@ -120,7 +99,7 @@ def dw_local(cursor):
 #	concelho = cursor.fetchall()
 
 	#for i in range(0,nTuplos[0][0]):
-	#	cursor.execute("insert into dw_local (id, stand_id, freguesia, concelho) values (%s, %s, %s, %s)", (i, stand_id[i][0], freguesia[i][0],concelho[i][0],))
+	#	cursor.execute("insert into dw_local (stand_id, freguesia, concelho) values (%s, %s, %s)", (stand_id[i][0], freguesia[i][0],concelho[i][0],))
 
 
 #id, hora, dia, mes
@@ -133,22 +112,45 @@ def dw_tempo(cursor):
 		dia = str(tempoI[i][0])[8] + str(tempoI[i][0])[9]
 		mes = str(tempoI[i][0])[5] + str(tempoI[i][0])[6]
 
-		cursor.execute("insert into dw_tempo (id, hora, dia, mes) values (%s, %s, %s, %s)", (i, hora, dia, mes))
+		cursor.execute("insert into dw_tempo (hora, dia, mes) values (%s, %s, %s)", (hora, dia, mes))
 	
 
+#taxi_id, tempo_id, localI_id, localF_id, nViagens, tempoTotal
 def dw_services(cursor):
 
-	cursor.execute("select final_ts - initial_ts from taxi_services")
-	tempo = cursor.fetchall()
-	
-	for i in range(len(tempoI)):
-		str(datetime.timedelta(seconds=tempo[0][0]))
+	#taxi_id
+	cursor.execute("select distinct taxi_id from taxi_services")
+	taxi_id = cursor.fetchall()
 
-	cursor.execute("select TIMESTAMP 'epoch' + initial_ts * INTERVAL '1 second' from taxi_services")
-	print cursor.fetchall()[0][0]	
-	cursor.execute("select TIMESTAMP 'epoch' + final_ts * INTERVAL '1 second' from taxi_services")
-	print cursor.fetchall()[0][0]
+	#numero de linhas que vao existir na tabela
+	nTuplos = len(taxi_id)
 	
+	for i in range(nTuplos):
+		#nViagens por taxi
+		cursor.execute("select count(*) from taxi_services where taxi_id = %s", (str(taxi_id[i][0]),))
+		nViagens = cursor.fetchall()		
 	
+		#tempo total por taxi
+		cursor.execute("select final_ts - initial_ts from taxi_services where taxi_id = %s", (str(taxi_id[i][0]),))
+		tempo = cursor.fetchall()
+		tempoTotal = str(datetime.timedelta(seconds=tempo[i][0]))
+
+		#cursor.execute("insert into dw_taxi_services (taxi_id, nViagens, tempoTotal) values (%s, %s, %s)", (taxi_id[i][0], nViagens[i][0], tempoTotal,))
+		break
+
+
 if __name__ == "__main__":
-	main()
+	conn_string = "dbname='guest' user='guest' host = '127.0.0.1' password=''"
+	conn = psycopg2.connect(conn_string)
+	conn.set_client_encoding('LATIN9')
+	cursor = conn.cursor()
+ 
+	cria_tabelas(cursor)
+	dw_stand(cursor)
+	#dw_local(cursor)
+	#dw_tempo(cursor)
+	#dw_services(cursor)
+
+	conn.commit()
+	cursor.close()
+	conn.close()
